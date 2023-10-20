478c5b55a490ec2a53982cba3b6160fb865b9ec0
root <root@DESKTOP-TAIC75H.localdomain>
2023-10-20 16:04:46 -0300

    Launch 'News'

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -2,6 +2,31 @@ import { Request, Response } from 'express'
 import { courseService } from '../services/courseService'
 
 export const coursesController = {
+    // GET /courses/featured
+  featured: async (req: Request, res: Response) => {
+    try {
+      const featuredCourses = await courseService.getRandomFeaturedCourses()
+      return res.json(featuredCourses)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  
+	// GET /courses/newest
+  newest: async (req: Request, res: Response) => {
+    try {
+      const newestCourses = await courseService.getTopTenNewest()
+      return res.json(newestCourses)
+    } catch (err) {
+			if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
   // GET /courses/:id
   show: async (req: Request, res: Response) => {
     const { id } = req.params
@@ -16,14 +41,6 @@ export const coursesController = {
     }
   },
 
-featured: async (req: Request, res: Response) => {
-  try {
-    const featuredCourses = await courseService.getRandomFeaturedCourses()
-    return res.json(featuredCourses)
-  } catch (err) {
-    if (err instanceof Error) {
-      return res.status(400).json({ message: err.message })
-    }
-  }
-}
+
+
 }
